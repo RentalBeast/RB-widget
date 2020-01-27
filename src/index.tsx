1c68c73889a9d5dee46b52132149bed0788eb705
sivakesh <psivakesh@gmail.com>
2020-01-27 11:23:34 -0600

    updated props

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {RBSearchWidget} from "./components/rb_search_widget";
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        <div><RBSearchWidget {...{...{settings: {state: 'Texas'}}, newTab: false}}/></div>,
+        <div><RBSearchWidget {...{settings: {state: 'Texas'}, newTab: false}}/></div>,
         document.getElementById('app-container'),
     )
 });
